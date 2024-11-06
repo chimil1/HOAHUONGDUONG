2ec189f3f72ce5f35f055b122fa9e02bfd6082cd
hungf <149678600+Hungpv18@users.noreply.github.com>
2024-11-06 16:00:55 +0700

    new

--- FE/src/frontend/admin/AddProduct.js
+++ FE/src/frontend/admin/AddProduct.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { fetchAddProduct } from "../actions/productAction";
+import { fetchAddProduct } from "../actions/unitActions";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
